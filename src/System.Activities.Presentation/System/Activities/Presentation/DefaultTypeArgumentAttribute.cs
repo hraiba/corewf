5f1cab7c5ee3999d5d36c9ae132823176fec17f7
Mohammad Hraiba <hraibam@gmail.com>
2019-11-15 20:41:02 +0100

    Added System.Activities.Presentation project.

---
+++ src/System.Activities.Presentation/System/Activities/Presentation/DefaultTypeArgumentAttribute.cs
@@ -0,0 +1,31 @@
+﻿//----------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//----------------------------------------------------------------
+
+namespace System.Activities.Presentation
+{
+    using System.Runtime;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Activities.Runtime;
+
+    [Fx.Tag.XamlVisible(false)]
+    [AttributeUsage(AttributeTargets.Class)]
+    public sealed class DefaultTypeArgumentAttribute : Attribute
+    {
+        Type type;
+
+        public DefaultTypeArgumentAttribute(Type type)
+        {
+            this.type = type;
+        }
+
+        [SuppressMessage("Microsoft.Naming", "CA1721:PropertyNamesShouldNotMatchGetMethods", Justification = "By design.")]
+        public Type Type
+        {
+            get
+            {
+                return this.type;
+            }
+        }
+    }
+}
---
+++ src/System.Activities.Presentation/System/Activities/Presentation/NullToBooleanConverter.cs
@@ -0,0 +1,25 @@
+//------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//------------------------------------------------------------
+
+namespace System.Activities.Presentation
+{
+    using System.Activities.Internals;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    //Null To Boolean Converter
+    internal sealed class NullToBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value != null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw FxTrace.Exception.AsError(new NotSupportedException());
+        }
+    }
+
+}
